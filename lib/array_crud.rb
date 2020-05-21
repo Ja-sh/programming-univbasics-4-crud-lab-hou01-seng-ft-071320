25b2be29d9e8f033c055b00cb0f7459f35947be7
Joshua Moore <morejoshua99@hotmail.com>
2020-05-21 21:04:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Joshua", "Jeff",  "Jerry",  "Jake"]
+  names = ["Joshua", "Jeff",  "Jerry",  "Jake"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  names << ("Mary")
 end
 
 def add_element_to_start_of_array(array, element)
